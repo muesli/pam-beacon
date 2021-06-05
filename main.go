cdadcd8fa9024aae5c64e34ad2a982c59f051e33
Christian Muehlhaeuser <muesli@gmail.com>
2021-06-05 23:41:44 +0200

    Update to latest go-bluetooth API

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ import (
 
 	"github.com/muesli/go-pam"
 	"github.com/muka/go-bluetooth/api"
-	"github.com/muka/go-bluetooth/emitter"
-	"github.com/muka/go-bluetooth/linux/btmgmt"
+	"github.com/muka/go-bluetooth/bluez/profile/adapter"
+	"github.com/muka/go-bluetooth/bluez/profile/device"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -27,7 +27,7 @@ var (
 
 const (
 	adapterID = "hci0"
-	timeout   = 10 * time.Second
+	timeout   = 30 * time.Second
 )
 
 func logError(args ...interface{}) {
@@ -40,40 +40,135 @@ func logErrorf(s string, args ...interface{}) {
 	time.Sleep(time.Second)
 }
 
-func printEnv() {
-	for _, e := range os.Environ() {
-		fmt.Println(e)
+func closeBluetooth() {
+	if err := api.Exit(); err != nil {
+		logError(err)
 	}
 }
 
-func closeBluetooth() {
-	api.StopDiscovery()
-	api.Exit()
-}
+func findDevice(addresses []string) bool {
+	log.Debugf("Looking for beacons %s...", addresses)
 
-func findDevice(address string) bool {
-	log.Debugf("Looking for beacon %s...", address)
-
-	a := btmgmt.NewBtMgmt(adapterID)
-	err := a.SetPowered(true)
+	a, err := adapter.GetAdapter(adapterID)
 	if err != nil {
 		logError(err)
 		return false
 	}
-
 	defer closeBluetooth()
-	go discoverDevices(address)
+
+	if p, err := a.GetPowered(); err != nil {
+		logError(err)
+		return false
+	} else if !p {
+		if err := a.SetPowered(true); err != nil {
+			logError(err)
+			return false
+		}
+	}
+
+	if monitorCachedDevices(a, addresses) {
+		return true
+	}
+
+	cancel, err := discoverDevices(a, addresses)
+	if err != nil {
+		logError(err)
+		return false
+	}
+	defer func() {
+		log.Debug("Canceling...")
+		cancel()
+	}()
 
 	select {
 	case <-time.After(timeout):
 		return false
-	case beacon := <-beaconCh:
-		log.Printf("Beacon %s found!", beacon)
+	case <-beaconCh:
 		return true
 	}
 }
 
-func monitorCachedDevices(beacon string) bool {
+func discoverDevices(a *adapter.Adapter1, addresses []string) (func(), error) {
+	if err := a.FlushDevices(); err != nil {
+		logError(err)
+		return nil, err
+	}
+
+	discovery, cancel, err := api.Discover(a, nil)
+	if err != nil {
+		logError(err)
+		return nil, err
+	}
+
+	log.Debugf("Discovered devices:")
+	go func() {
+		for ev := range discovery {
+			go func(ev *adapter.DeviceDiscovered) {
+				dev, err := device.NewDevice1(ev.Path)
+				if err != nil {
+					log.Errorf("%s: %s", ev.Path, err)
+					return
+				}
+				if dev == nil {
+					log.Errorf("%s: not found", ev.Path)
+					return
+				}
+
+				if addr, ok := checkDevice(dev, addresses); ok {
+					go func(addr string) {
+						// we sent this in a go-routine to prevent this function
+						// from dead-locking a bluetooth shutdown
+						beaconCh <- addr
+					}(addr)
+				}
+			}(ev)
+		}
+	}()
+
+	return cancel, nil
+}
+
+func checkDevice(dev *device.Device1, addresses []string) (string, bool) {
+	props, err := dev.GetProperties()
+	if err != nil {
+		logErrorf("%s: Failed to get properties: %s", dev.Path(), err.Error())
+		return "", false
+	}
+	log.Debugf("name=%s addr=%s rssi=%d trusted=%t tx=%d connected=%t",
+		props.Name, props.Address, props.RSSI, props.Trusted, props.TxPower, props.Connected)
+
+	var addr string
+	for _, v := range addresses {
+		if strings.EqualFold(props.Address, v) {
+			addr = props.Address
+			break
+		}
+	}
+	if addr == "" {
+		return "", false
+	}
+
+	if !props.Connected {
+		// check we can actually connect to the device
+		log.Debugf("Connecting to %s...", props.Address)
+		err = dev.Connect()
+		if err != nil {
+			logErrorf("%s: Failed to connect: %s", dev.Path(), err.Error())
+			return "", false
+		}
+
+		log.Debugf("Disconnecting from %s...", props.Address)
+		err = dev.Disconnect()
+		if err != nil {
+			logErrorf("%s: Failed to disconnect: %s", dev.Path(), err.Error())
+		}
+	}
+
+	log.Printf("Beacon %s found!", props.Address)
+	return props.Address, true
+}
+
+func monitorCachedDevices(api *adapter.Adapter1, addresses []string) bool {
 	devices, err := api.GetDevices()
 	if err != nil {
 		logError(err)
@@ -82,81 +177,12 @@ func monitorCachedDevices(beacon string) bool {
 
 	log.Debugf("Cached devices:")
 	for _, dev := range devices {
-		if checkDevice(dev, beacon) {
+		if _, ok := checkDevice(dev, addresses); ok {
 			return true
 		}
 	}
 
 	return false
-}
-
-func discoverDevices(beacon string) {
-	if monitorCachedDevices(beacon) {
-		return
-	}
-
-	log.Debugf("Discovered devices:")
-
-	api.StopDiscovery()
-	time.Sleep(100 * time.Millisecond)
-
-	err := api.On("discovery", emitter.NewCallback(func(ev emitter.Event) {
-		discoveryEvent := ev.GetData().(api.DiscoveredDeviceEvent)
-		dev := discoveryEvent.Device
-		checkDevice(*dev, beacon)
-	}))
-	if err != nil {
-		logError(err)
-		return
-	}
-
-	err = api.StartDiscovery()
-	if err != nil {
-		logError(err)
-		return
-	}
-}
-
-func checkDevice(dev api.Device, beacon string) bool {
-	props, err := dev.GetProperties()
-	if err != nil {
-		logErrorf("%s: Failed to get properties: %s", dev.Path, err.Error())
-		return false
-	}
-	log.Debugf("name=%s addr=%s rssi=%d trusted=%t tx=%d connected=%t",
-		props.Name, props.Address, props.RSSI, props.Trusted, props.TxPower, props.Connected)
-
-	if props.Address != beacon {
-		return false
-	}
-
-	if props.Connected || props.RSSI != 0 {
-		beaconCh <- props.Address
-		return true
-	}
-
-	watchDevice(dev, beacon)
-	return false
-}
-
-func watchDevice(dev api.Device, beacon string) {
-	err := dev.On("changed", emitter.NewCallback(func(ev emitter.Event) {
-		changed := ev.GetData().(api.PropertyChangedEvent)
-		// spew.Dump(changed.Properties)
-		// spew.Dump(changed.Device.GetAllServicesAndUUID())
-		if changed.Properties.Address != beacon {
-			return
-		}
-		log.Debugf("%s: %d %s %+v", changed.Properties.Name, changed.Properties.RSSI, changed.Field, changed.Value)
-		if (changed.Field == "RSSI" && changed.Value.(int16) != 0) ||
-			(changed.Field == "Connected" && changed.Value.(bool)) {
-			beaconCh <- changed.Properties.Address
-			return
-		}
-	}))
-	if err != nil {
-		logError(err)
-	}
 }
 
 //export goAuthenticate
@@ -185,30 +211,10 @@ func goAuthenticate(handle *C.pam_handle_t, flags C.int, argv []string) C.int {
 		}
 	}
 
-	if findDevice(addrs[0]) {
+	if findDevice(addrs) {
 		return C.PAM_SUCCESS
 	}
 	return C.PAM_AUTH_ERR
-
-	/*
-		cmd := exec.Command("dbus-send", "--system", "--dest=org.bluez", "--print-reply", "/org/bluez/hci0", "org.freedesktop.DBus.Properties.Set", "string:org.bluez.Adapter1", "string", ":Discoverable", "variant:boolean:true")
-		out, err := cmd.Output()
-		if err != nil {
-			fmt.Println(err)
-		}
-	*/
-	/*
-		cmd := exec.Command("hcitool", "name", beaconAddress)
-		out, err := cmd.Output()
-		if err != nil {
-			fmt.Println(err)
-			return C.PAM_AUTH_ERR
-		}
-		if len(strings.TrimSpace(string(out))) == 0 {
-			return C.PAM_AUTH_ERR
-		}
-		return C.PAM_SUCCESS
-	*/
 }
 
 //export setCred
@@ -226,7 +232,7 @@ func main() {
 		fmt.Println("usage: pam-beacon <mac-addr>")
 		os.Exit(2)
 	}
-	if !findDevice(os.Args[1]) {
+	if !findDevice(os.Args[1:]) {
 		os.Exit(1)
 	}
 }
